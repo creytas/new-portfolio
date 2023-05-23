36c75c316d5647978a4c5921ea98fc33304f8847
koumishop15 <koumishop@gmail.com>
2023-05-23 17:15:35 +0100

    feat: set poppins as primary font

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import './globals.css'
-import { Roboto } from 'next/font/google'
+import { Poppins } from 'next/font/google'
 
-const roboto = Roboto({ subsets: ['latin'], weight: ['100', '300', '400', '500', '700', '900'] })
+const poppins = Poppins({ subsets: ['latin'], weight: ['100', '300', '400', '500', '700', '900'] })
 
 export const metadata = {
   title: 'Randy Buhendwa - Portfolio',
@@ -11,7 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={roboto.className}>{children}</body>
+      <body className={poppins.className}>{children}</body>
     </html>
   )
 }
