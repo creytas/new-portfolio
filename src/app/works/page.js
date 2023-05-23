a667da40ce8e4afae5b2dc1e64c6edb1c1b206cb
Randy Buhendwa <creytas@gmail.com>
2023-05-23 11:20:34 +0100

    chore: initialize the project

---
+++ src/app/works/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image"
+import Link from "next/link"
+
+export default function Work() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <h1>WORKS I'VE DONE...</h1>
+        <Link href='/'>BACK</Link>
+    </main>
+  );
+}
