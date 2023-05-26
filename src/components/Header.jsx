9bc3677639bb33fcb4089a4af7c58097099687b9
koumishop15 <koumishop@gmail.com>
2023-05-26 17:08:05 +0100

    feat: create header for medium screens

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,12 +3,21 @@ import { Icon } from "@iconify/react"
 
 const HeaderBar = ({isLight, setIsLight}) =>{
     return(
-        <header className="w-screen flex justify-end">
-            <div className="w-1/4 h-[95px] bg-[url('/headerSmBg.svg')] bg-no-repeat">
+        <header className="w-screen flex justify-end md:pr-16">
+            <div className="w-1/4 h-[95px] md:h-[304px] bg-[url('/headerSmBg.svg')] md:bg-[url('/headerMdBg.svg')] bg-no-repeat">
 
             </div>
-            <div className="w-3/4 min-h-[74px] flex justify-end p-4">
-                <button onClick={()=>setIsLight(!isLight)}><Icon icon={ isLight ? "solar:moon-broken" : "solar:sun-broken"} className={ isLight ? "w-[30px] h-[30px]" : "w-[40px] h-[40px] text-white"} /></button>
+            <div className="w-3/4 min-h-[74px] flex justify-end md:items-start p-4">
+                <div className="w-full flex items-center justify-end">
+                    <div className="hidden md:flex md:mr-12 md:space-x-12">
+                        <span className="flex items-center hover:text-primary"><Icon icon="bx:home-smile" className="w-[30px] h-[30px] mr-1" /> Home</span>
+                        <span className="flex items-center hover:text-primary"><Icon icon="bx:user" className="w-[30px] h-[30px] mr-1" /> About</span>
+                        <span className="flex items-center hover:text-primary"><Icon icon="material-symbols:computer-outline" className="w-[30px] h-[30px] mr-1" /> Portfolio</span>
+                        <span className="flex items-center hover:text-primary"><Icon icon="iconoir:mail" className="w-[30px] h-[30px] mr-1" /> Contact</span>
+                    </div>                
+                    <button onClick={()=>setIsLight(!isLight)}><Icon icon={ isLight ? "solar:moon-broken" : "solar:sun-broken"} className={ isLight ? "w-[30px] h-[30px]" : "w-[40px] h-[40px] text-white"} /></button>
+                </div>
+
             </div>
         </header>
     )    
