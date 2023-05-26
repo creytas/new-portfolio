67ad6c926cbf4b856197812b8d519f65d410b3b1
koumishop15 <koumishop@gmail.com>
2023-05-26 12:04:24 +0100

    feat: add header backround

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,8 +4,11 @@ import { Icon } from "@iconify/react"
 const HeaderBar = ({isLight, setIsLight}) =>{
     return(
         <header className="border w-screen flex justify-end">
-            <div className="w-1/2 min-h-[74px] flex justify-end p-4 border border-red">
-                <button onClick={()=>setIsLight(!isLight)}><Icon icon={ isLight ?  "solar:sun-broken": "solar:moon-broken"} className={ isLight ? "w-[40px] h-[40px]" : "w-[30px] h-[30px] text-white"} /></button>
+            <div className="w-1/2 h-[95px] border border-red bg-[url('/headerSmBg.svg')] bg-no-repeat">
+
+            </div>
+            <div className="w-1/2 min-h-[74px] flex justify-end p-4">
+                <button onClick={()=>setIsLight(!isLight)}><Icon icon={ isLight ? "solar:moon-broken" : "solar:sun-broken"} className={ isLight ? "w-[30px] h-[30px]" : "w-[40px] h-[40px] text-white"} /></button>
             </div>
         </header>
     )    
