b3355aed2d1b0574fa6c27e350b4057002c3e9ad
koumishop15 <koumishop@gmail.com>
2023-05-26 12:28:51 +0100

    feat: create footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+
+import { Icon } from "@iconify/react"
+
+const FooterBar = () =>{
+    return(
+        <footer className="w-screen flex justify-end items-center">
+            <strong className="w-3/4 min-h-[74px] space-x-1 flex justify-center p-6 font-thin text-[12.5px]">
+                <span className="font-bold">© Imagin’Soft 2022</span><span> - All rights reserved </span>
+            </strong>
+            <div className="w-1/4 h-[95px] bg-[url('/footerSmBg.svg')] bg-no-repeat bg-right">
+            </div>
+        </footer>
+    )    
+}
+export default FooterBar
