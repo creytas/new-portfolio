96e5dddcb8222b25749ae26700a721b4051a4f50
koumishop15 <koumishop@gmail.com>
2023-05-26 12:29:29 +0100

    style: change component size to screen

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,11 +3,11 @@ import { Icon } from "@iconify/react"
 
 const HeaderBar = ({isLight, setIsLight}) =>{
     return(
-        <header className="border w-screen flex justify-end">
-            <div className="w-1/2 h-[95px] border border-red bg-[url('/headerSmBg.svg')] bg-no-repeat">
+        <header className="w-screen flex justify-end">
+            <div className="w-1/4 h-[95px] bg-[url('/headerSmBg.svg')] bg-no-repeat">
 
             </div>
-            <div className="w-1/2 min-h-[74px] flex justify-end p-4">
+            <div className="w-3/4 min-h-[74px] flex justify-end p-4">
                 <button onClick={()=>setIsLight(!isLight)}><Icon icon={ isLight ? "solar:moon-broken" : "solar:sun-broken"} className={ isLight ? "w-[30px] h-[30px]" : "w-[40px] h-[40px] text-white"} /></button>
             </div>
         </header>
