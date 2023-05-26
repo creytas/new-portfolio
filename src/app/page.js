b11062587ae90d3430d09c6f21beddc7e9659afb
koumishop15 <koumishop@gmail.com>
2023-05-26 12:29:54 +0100

    feat: insert footer

--- src/app/page.js
+++ src/app/page.js
@@ -2,13 +2,15 @@
 import { useState } from "react"
 import About from "@/components/About"
 import HeaderBar from "@/components/Header";
+import FooterBar from "@/components/Footer";
 
 export default function Home() {
   const [isLight, setIsLight] = useState(true)
   return (
-    <main className={`${isLight? "bg-white bg-opacity-90 text-black" : "bg-navy text-white"} flex min-h-screen flex-col items-center justify-between pb-24 px-24 text-black`}>
+    <main className={`${isLight? "bg-white bg-opacity-90 text-black" : "bg-navy text-white"} flex min-h-screen flex-col items-center justify-between px-24 text-black`}>
       <HeaderBar isLight={isLight} setIsLight={setIsLight} />
       <About isLight={isLight}/>
+      <FooterBar/>
     </main>
   );
 }
