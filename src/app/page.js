bb345c22ee06ac26b62c7a61d89efef6bb16c043
koumishop15 <koumishop@gmail.com>
2023-06-02 13:07:47 +0100

    style: set dark mode as default theme

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@ import HeaderBar from "@/components/Header";
 import FooterBar from "@/components/Footer";
 
 export default function Home() {
-  const [isLight, setIsLight] = useState(true)
+  const [isLight, setIsLight] = useState(false)
   return (
     <main className={`${isLight? "bg-white bg-opacity-90 text-black" : "bg-navy text-white"} flex min-h-screen flex-col items-center justify-between px-24 text-black`}>
       <HeaderBar isLight={isLight} setIsLight={setIsLight} />
