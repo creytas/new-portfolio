8585c5ba375206a36e5b67c8b7be7865417db3e7
koumishop15 <koumishop@gmail.com>
2023-06-16 10:56:29 +0100

    style: change header bg-opacity on hover

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,7 @@ const HeaderBar = ({isLight, setIsLight}) =>{
             <div className="w-1/4 h-[95px] bg-[url('/headerSmBg.svg')] md:hidden bg-no-repeat ">
 
             </div>
-            <div className={`w-3/4 md:w-screen min-h-[74px] flex justify-end md:items-start p-4 md:pr-16 md:fixed ${isLight ? "md:bg-secondary/70 md:hover:bg-secondary" : "md:bg-alt-navy/70 md:hover:bg-navy"} md:z-20`}>
+            <div className={`w-3/4 md:w-screen min-h-[74px] flex justify-end md:items-start p-4 md:pr-16 md:fixed ${isLight ? "md:bg-secondary/70 md:hover:bg-secondary/95" : "md:bg-alt-navy/70 md:hover:bg-navy/95"} md:z-20`}>
                 <div className="w-full flex items-center justify-end">
                     <div className="border-l-4 border-white w-[51%] flex justify-start"><Image src="/rb_logo_1.png" alt="rbportfolio logo" width={80} height={80} /></div>
                     <div className="hidden md:flex md:mr-12 md:space-x-12">
